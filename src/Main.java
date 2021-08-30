54018a1cd18cad0131a4d667117de8bf3bf95b7c
michal.walkowiak93@gmail.com <michal.walkowiak93@gmail.com>
2021-08-30 11:23:44 +0200

    refactor

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,22 @@
 import java.util.Scanner;
+import java.util.function.BiPredicate;
 
 public class Main {
     private static String SPACE = " ";
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         String input = in.nextLine();
+
         String positionFirstRook = input.split(SPACE)[0];
         String positionSecondRook = input.split(SPACE)[1];
-
-        if (positionFirstRook.charAt(0) == positionSecondRook.charAt(0) || positionFirstRook.charAt(1) == positionSecondRook.charAt(1)){
+        if (positionChecker.test(positionFirstRook, positionSecondRook)) {
             System.out.println("TAK");
         } else {
             System.out.println("NIE");
         }
     }
+
+    private static BiPredicate<String, String> positionChecker = (firstRook, secondRook) ->
+            firstRook.charAt(0) == secondRook.charAt(0) || firstRook.charAt(1) == secondRook.charAt(1);
 }
