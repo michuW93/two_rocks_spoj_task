b22d952243f84d20a61275c1dd1a80085c9886b7
michal.walkowiak93@gmail.com <michal.walkowiak93@gmail.com>
2021-08-30 11:01:23 +0200

    small refactor

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,12 @@
 import java.util.Scanner;
 
 public class Main {
+    private static String SPACE = " ";
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         String input = in.nextLine();
-        String positionFirstRook = input.split(" ")[0];
-        String positionSecondRook = input.split(" ")[1];
+        String positionFirstRook = input.split(SPACE)[0];
+        String positionSecondRook = input.split(SPACE)[1];
 
         if (positionFirstRook.charAt(0) == positionSecondRook.charAt(0) || positionFirstRook.charAt(1) == positionSecondRook.charAt(1)){
             System.out.println("TAK");
